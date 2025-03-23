733ba95ca25bea42d0bf8aaa732b396cb85ec471
Vladimir-Z <vladimir.zverev@urfu.ru>
2025-03-23 11:41:20 +0500

    Добавление файлов hw4 из shanon ftp

---
+++ 07-iterators/hw/hw4/hw4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import unittest
+
+
+def make_stat():
+    # TODO: make your class instance and return it
+
+
+class LogStatTests(unittest.TestCase):
+    # TODO: add unit tests
